cb958449e10f16cfc903e4017468ae4dab89fb13
Brett M. Morris <bmmorris@stsci.edu>
2023-03-24 16:56:15 -0400

    init commit

---
+++ docs/conf.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# This file does only contain a selection of the most common options. For a
+# full list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'sfear'
+copyright = '2023, Brett M. Morris'
+author = 'Brett M. Morris'
+
+# The full version, including alpha/beta/rc tags
+from sfear import __version__
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.inheritance_diagram',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
+    'sphinx.ext.mathjax',
+    'sphinx_automodapi.automodapi',
+    'sphinx_automodapi.smart_resolver',
+    "matplotlib.sphinxext.plot_directive",
+    "numpydoc",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+# templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# -- Options for intersphinx extension ---------------------------------------
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'https://docs.python.org/': None}
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_book_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+
+numpydoc_show_class_members = False
+autodoc_inherit_docstrings = True
+
+autosectionlabel_prefix_document = True
+autoclass_content = 'both'
+
+html_context = {
+    "display_github": True,
+    "github_user": "bmorris3",
+    "github_repo": "sfear",
+    "github_version": "main",
+    "conf_py_path": "docs/",
+}
+
+html_theme_options = {
+    "logo_only": True,
+    "use_download_button": True,
+    "repository_url": "https://github.com/bmorris3/sfear",
+    "repository_branch": "main",
+    "path_to_docs": "docs",
+}
+
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+sfear Documentation
+-------------------
+
+This is the documentation for sfear.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
